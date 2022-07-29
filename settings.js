4b574a216fd474c9b41b3e21e94e93d4bae9237f
go31jo <109923393+go31jo@users.noreply.github.com>
2022-07-29 16:56:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,42 +19,42 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is  ツ𝐁𝐎𝐓 𝐇𝐀𝐒𝐇𝐀࿐
+My Creator is  ツ𝐁𝐎𝐓 𝐇𝐀𝐒𝐇𝐀࿐` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+771337628'] //Owner number in aive msg
+global.premium = ['+771337628'] //Owner Number info
+global.ownernomer = '+771337628' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = ' ツ𝐁𝐎𝐓 𝐇𝐀𝐒𝐇𝐀࿐' //Owner Name
+global.botname = ' ツ𝐁𝐎𝐓 𝐇𝐀𝐒𝐇𝐀࿐' //Bot Name
 
 global.button = '☰ GITHUB ☰' //Costomize A Button Name In Alive Message
 global.btnurl = 'https://github.com/prabhasha2006' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = ' ツ𝐁𝐎𝐓 𝐇𝐀𝐒𝐇𝐀࿐' //Bot Pacage Name
+global.author = ' ツ𝐁𝐎𝐓 𝐇𝐀𝐒𝐇𝐀࿐' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = '©_Hashan.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
 global.ig = 'https://github.com/prabhasha2006' //Github Account!
 
 global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
 	
-    success: 'Done ✅',
+    success: 'දැන් සැපත ✅',
     admin: 'Only Admins can use it!😉',
     botAdmin: 'Bot wants to be an Admin😥',
     owner: 'Only Owner can use this!😉',
     group: 'Feature Used Only For Groups!',
     private: 'This Features Only For Private Chat!',
     bot: 'This Feature Is Only For Bot😝',
-    wait: '👉👈Pleas Wait...',
+    wait: '👉👈 ඉදාම් ටිකක්',
     error: 'Error!!! Maybe Api Key Is Expired🤔!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
 }
